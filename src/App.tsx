9dbe8d6e3baaa25ee392a03d80652214286f8d82
KelaiAffaf <ea_kelai@esi.dz>
2025-02-19 00:58:32 +0100

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/tailwind.css";
 import { TodoList } from "./components/todo/todo-list";
 import Counter from "./components/counter/counter-optmized";
